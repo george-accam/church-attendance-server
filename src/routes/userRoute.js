56ce0455e026127e0b51eddb7eb21983f6fb53c9
Buertey Accam <manreg814@gmail.com>
2025-02-21 15:11:03 +0000

    feat: add search functionality for users by name or phone number

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/middlewareToken.js";
-import { registerUser, userLogin, getAllUsers, getUserById, updateUser, deleteUser } from "../controllers/userController.js";
+import { registerUser, userLogin, getAllUsers, getUserById, updateUser, deleteUser, searchUser } from "../controllers/userController.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
-router.post("/login", userLogin);
-router.get("/users", getAllUsers);
-router.get("/user-id", verifyToken, getUserById);
-router.put("/update-user/:id", updateUser);
-router.delete("/delete-user/:id", deleteUser);
+router.post("/register", registerUser); // register user
+router.post("/login", userLogin); // login user
+router.get("/users", getAllUsers); // get all users
+router.get("/search-users", searchUser); // search user
+router.get("/user-id", verifyToken, getUserById); // get user by id
+router.put("/update-user/:id", updateUser); // update user
+router.delete("/delete-user/:id", deleteUser); // delete user
 
 export default router;
