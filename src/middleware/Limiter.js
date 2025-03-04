ffe53c403e834fe39f00dd762c23eff6b3a0bd9d
Buertey Accam <manreg814@gmail.com>
2025-03-04 23:17:33 +0000

    fix: update rate limiting parameters for login attempts to 10 minutes

--- src/middleware/Limiter.js
+++ src/middleware/Limiter.js
@@ -1,7 +1,7 @@
 import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
-    windowMs: 2 * 60* 1000, // 15 minutes
+    windowMs: 10 * 60* 1000, // 10 minutes
     max: 5, // limit each IP to 100 requests per windowMs
-    message: "Too many attempt to login, please try again after 15 minutes",
+    message: "Too many attempt to login, please try again after 10 minutes",
 });
