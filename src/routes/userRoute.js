ba4a2adbbf21d3749f1cd76ef4aee44f6a44ca3c
Buertey Accam <manreg814@gmail.com>
2025-03-04 22:47:01 +0000

    feat: implement rate limiting for login attempts

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/middlewareToken.js";
+import { limiter } from "../middleware/Limiter.js";
 import { registerUser, userLogin, getAllUsers, getUserById, updateUser, deleteUser, searchUser } from "../controllers/userController.js";
 
 const router = Router();
+router.use( "/login",limiter);
 
 router.post("/register", registerUser); // register user
-router.post("/login", userLogin); // login user
+router.post("/login", limiter, userLogin); // login user
 router.get("/users", getAllUsers); // get all users
 router.get("/search-users", searchUser); // search user
 router.get("/user-id", verifyToken, getUserById); // get user by id
