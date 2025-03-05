35a0890fd79e063eefd6932b0640eb4c5529a216
Buertey Accam <manreg814@gmail.com>
2025-03-05 01:35:10 +0000

    fix: correct rate limiting configuration to use 'limit' instead of 'max'

--- src/middleware/Limiter.js
+++ src/middleware/Limiter.js
@@ -2,11 +2,10 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 10 * 60* 1000, // 10 minutes
-    max: 5, // limit each IP to 5 requests per windowMs
+    limit: 5, // limit each IP to 5 requests per windowMs
     message: "Too many attempt to login, please try again after 10 minutes",
     skipSuccessfulRequests: true,
     handler: (req, res) => {
         res.status(429).json({ success: false, message: "Too many failed attempt to login, please try again after 10 minutes" });
     },
-    keyGenerator: (req) => req.ip,
 });
