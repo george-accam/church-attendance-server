cb43a744cc27996aa27ef51bea6ab6cec9ce453f
Buertey Accam <manreg814@gmail.com>
2025-03-05 03:45:34 +0000

    fix: update rate limiting configuration to set limit to 3 requests

--- src/middleware/Limiter.js
+++ src/middleware/Limiter.js
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 10 * 60* 1000,
-    limit: 5, 
+    limit: 3, 
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many attempt to login, please try again after 10 minutes",
