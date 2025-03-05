1ff4321d34d7472d83d0e2fc19ea3a583731af10
Buertey Accam <manreg814@gmail.com>
2025-03-05 01:28:19 +0000

    fix: enable skipping successful requests in rate limiting handler

--- src/middleware/Limiter.js
+++ src/middleware/Limiter.js
@@ -4,6 +4,7 @@ export const limiter = rateLimit({
     windowMs: 10 * 60* 1000, // 10 minutes
     max: 5, // limit each IP to 5 requests per windowMs
     message: "Too many attempt to login, please try again after 10 minutes",
+    skipSuccessfulRequests: true,
     handler: (req, res) => {
         res.status(429).json({ success: false, message: "Too many failed attempt to login, please try again after 10 minutes" });
     },
