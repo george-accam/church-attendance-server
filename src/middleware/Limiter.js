83b42df3a77a60bae0e7a516f167f5bda66036bd
Buertey Accam <manreg814@gmail.com>
2025-03-05 15:30:57 +0000

    feat: implement rate limiter middleware and update login route to use it

--- src/middleware/Limiter.js
+++ src/middleware/Limiter.js
@@ -3,6 +3,8 @@ import { rateLimit } from "express-rate-limit";
 export const limiter = rateLimit({
     windowMs: 10000,
     limit: 3, 
+    standardHeaders: true,
+    legacyHeaders: false,
     handler: (req, res) => {
         res.status(429).json({ success: false, message: "Too many failed attempt to login, please try again after 10 minutes" });
     },
