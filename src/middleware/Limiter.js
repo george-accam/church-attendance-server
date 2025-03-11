e34530b5a94714ac72ee7f6c927b1bd123227d0e
Buertey Accam <manreg814@gmail.com>
2025-03-11 20:00:55 +0000

    fix: update rate limiting window to 10 minutes

--- src/middleware/Limiter.js
+++ src/middleware/Limiter.js
@@ -1,7 +1,7 @@
 import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
-    windowMs: 10000,
+    windowMs: 10 * 60 * 1000,
     limit: 3, 
     standardHeaders: true,
     legacyHeaders: false,
